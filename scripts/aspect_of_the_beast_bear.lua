9b54524466e786a6b2c00809ba5eba30ac2de03f
JustinFreitas <justin.s.freitas@me.com>
2022-02-08 04:32:04 -0700

    Use local storage for old function.  Minor formatting refactors.

--- scripts/aspect_of_the_beast_bear.lua
+++ scripts/aspect_of_the_beast_bear.lua
@@ -1,12 +1,7 @@
--- (c) Copyright Justin Freitas 2021+ except where explicitly stated otherwise.
--- Fantasy Grounds is Copyright (c) 2004-2021 SmiteWorks USA LLC.
--- Copyright to other material within this file may be held by other Individuals and/or Entities.
--- Nothing in or from this LUA file in printed, electronic and/or any other form may be used, copied,
--- transmitted or otherwise manipulated in ANY way without the explicit written consent of
--- Justin Freitas or, where applicable, any and all other Copyright holders.
+local getEncumbranceMult_orig
 
 function onInit()
-	CharManager.getEncumbranceMultAspectOfTheBeastBear = CharManager.getEncumbranceMult
+	getEncumbranceMult_orig = CharManager.getEncumbranceMult
 	CharManager.getEncumbranceMult = getEncumbranceMultOverride
 
 	local featureNamePath = "charsheet.*.featurelist.*.name"
@@ -17,7 +12,7 @@ end
 -- This is entered on strength change or trait change (not feature) due to the way record_char_inventory.xml works.
 -- See: <number_linked name="encumbrancebase" source="encumbrance.encumbered">
 function getEncumbranceMultOverride(nodeChar)
-	local mult = CharManager.getEncumbranceMultAspectOfTheBeastBear(nodeChar)
+	local mult = getEncumbranceMult_orig(nodeChar)
 	if isBarbarianOfLevelSixOrHigher(nodeChar) and hasAspectOfTheBeastBear(nodeChar) then
 		mult = mult * 2
 	end
@@ -27,7 +22,8 @@ end
 
 function hasAspectOfTheBeastBear(nodeChar)
 	for _, nodeFeature in pairs(DB.getChildren(nodeChar, "featurelist")) do
-		if string.match(DB.getValue(nodeFeature, "name", ""):lower(), "^%W*aspect%W+of%W+the%W+beast%W*bear%W*$") then
+		local name = DB.getValue(nodeFeature, "name", ""):lower()
+		if string.match(name, "^%W*aspect%W+of%W+the%W+beast%W*bear%W*$") then
 			return true
 		end
 	end
@@ -37,7 +33,8 @@ end
 
 function isBarbarianOfLevelSixOrHigher(nodeChar)
 	for _, nodeClass in pairs(DB.getChildren(nodeChar, "classes")) do
-		if DB.getValue(nodeClass, "name", ""):lower() == "barbarian" and DB.getValue(nodeClass, "level", 0) >= 6 then
+		if DB.getValue(nodeClass, "name", ""):lower() == "barbarian" and
+		   DB.getValue(nodeClass, "level", 0) >= 6 then
 			return true
 		end
 	end
@@ -55,9 +52,12 @@ function onFeatureNameAddOrUpdate(nodeFeatureName)
 end
 
 function updateInventoryPaneEncumbranceBaseIfLoaded(w)
-	if not (w and w.inventory and w.inventory.subwindow and w.inventory.subwindow.contents and w.inventory.subwindow.contents.subwindow
-			and w.inventory.subwindow.contents.subwindow.encumbrancebase
-			and w.inventory.subwindow.contents.subwindow.encumbrancebase.onTraitsUpdated) then return end
+	if not (w and w.inventory
+			  and w.inventory.subwindow
+			  and w.inventory.subwindow.contents
+			  and w.inventory.subwindow.contents.subwindow
+			  and w.inventory.subwindow.contents.subwindow.encumbrancebase
+			  and w.inventory.subwindow.contents.subwindow.encumbrancebase.onTraitsUpdated) then return end
 
 	-- See: <number_linked name="encumbrancebase" source="encumbrance.encumbered">
 	w.inventory.subwindow.contents.subwindow.encumbrancebase.onTraitsUpdated()
