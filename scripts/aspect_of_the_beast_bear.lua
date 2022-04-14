c133f3f811a5230b6048c4258b46d945bd84537b
JustinFreitas <justin.s.freitas@me.com>
2022-04-13 22:18:48 -0600

    equal to

--- scripts/aspect_of_the_beast_bear.lua
+++ scripts/aspect_of_the_beast_bear.lua
@@ -20,7 +20,7 @@ function checkBearFromFGU()
 	local nMajor, nMinor, nPatch = Interface.getVersion()
 	if nMajor >= 5 then return true end
 	if nMajor == 4 and nMinor >= 2 then return true end
-	return nMajor == 4 and nMinor >= 1 and nPatch >= 14
+	return nMajor == 4 and nMinor == 1 and nPatch >= 14
 end
 
 -- This is entered on strength change or trait change (not feature)
